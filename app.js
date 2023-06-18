b8d1af28dacdfab22061eccd489866cae1cd0e7a
Asaduzzaman Himel <66587375+asaduzzaman69@users.noreply.github.com>
2023-06-18 18:14:20 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
-app.get('*',  createProxyMiddleware({ 
-    target: 'https://huehq.com/', // target host
-    changeOrigin: true, // needed for virtual hosted sites
-}));
+app.get('/',  (req,res) => {
+
+    res.send('Hello world')
+});
 
 
 module.exports = app
