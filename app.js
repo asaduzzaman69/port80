c692a44e9718a86b9996c3aa822233b10aa705b9
Asaduzzaman Himel <66587375+asaduzzaman69@users.noreply.github.com>
2023-06-18 18:07:03 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 app.get('*',  createProxyMiddleware({ 
-    target: 'https://portal-a229a.web.app/login', // target host
+    target: 'https://huehq.com/', // target host
     changeOrigin: true, // needed for virtual hosted sites
 }));
 
