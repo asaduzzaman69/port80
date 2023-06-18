a7ed96bd603dc4f3ddcd3279130c72ad0e4548c4
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2023-06-18 21:27:38 +0600

    added proxy with database

--- app.js
+++ app.js
@@ -15,12 +15,18 @@ const db = admin.firestore();
 app.use("*", async (req, res, next) => {
   const host = req.headers.host;
 
+  console.log({
+    host,
+  });
   // Fetch the portal from the database
   const portalsSnapshot = await db
     .collection("portals")
     .where("customDomain", "==", host)
     .get();
 
+    console.log({
+      isEmpty: portalsSnapshot.empty,
+    });
   // If there's no portal for this host, return a 404
   if (portalsSnapshot.empty) {
     res.status(404).send("Not found");
@@ -29,7 +35,7 @@ app.use("*", async (req, res, next) => {
 
   // Get the first matched portal's data
   const portalData = portalsSnapshot.docs[0].data();
-
+console.log({ portalData });
   // Extract the target URL from the portal data.
   // This assumes that the target URL is stored in a field named 'targetUrl' in each portal document.
   // If the field has a different name, make sure to change 'targetUrl' to the correct field name.
